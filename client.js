bb70676de98d831466642d4c26465a04d9360050
CamilleAbella <camille.abella@hotmail.fr>
2020-11-11 21:30:33 +0100

    disable everyone mention only

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ const db = require("./db")
 const utils = require("./utils")
 
 const client = new Discord.Client({
-  disableMentions: "all",
+  disableMentions: "everyone",
   ws: {
     intents: [
       "GUILDS",
