5987b976d74b77885746c2f66cf56d168c51466d
CamilleAbella <camille.abella@hotmail.fr>
2020-11-19 03:13:00 +0100

    added build script on deployment script

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -14,7 +14,7 @@ const command: app.Command = {
     const timer = Date.now()
 
     try {
-      await exec("git pull && npm i")
+      await exec("git pull && npm i && npm run build")
       await subject.edit(
         `Déploiement réussi ! <:yay:557124850326437888>\n*Effectué en ${
           Date.now() - timer
