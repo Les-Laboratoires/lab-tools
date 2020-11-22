04537e6f63c5b079b777b32480242b8d13aa78f5
CamilleAbella <camille.abella@hotmail.fr>
2020-11-22 14:22:59 +0100

    added scores database

--- src/app/database.ts
+++ src/app/database.ts
@@ -4,7 +4,11 @@ export const globals = new Enmap<string, any>({
   name: "globals",
 })
 
-export const coolDowns = new Enmap<string, { time: number; trigger: boolean }>()
+export const scores = new Enmap<string, Score>({
+  name: "scores"
+})
+
+export const coolDowns = new Enmap<string, CoolDown>()
 
 // /** Enmap[Guild, Prefix] */
 // export const prefixes = new Enmap<Discord.Snowflake, string>({
@@ -15,3 +19,17 @@ export const coolDowns = new Enmap<string, { time: number; trigger: boolean }>()
 // export const muted = new Enmap<Discord.Snowflake, Discord.Snowflake[]>({
 //  name: "muted"
 // })
+
+export interface CoolDown {
+  time: number
+  trigger: boolean
+}
+
+export interface Score {
+  grammarNazi: number
+  reputation: number
+  helping: number
+  money: number
+  bite: number
+  lol: number
+}
