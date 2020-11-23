b9fa6ff0411256568056d0cb308d876e8b8d9208
CamilleAbella <camille.abella@hotmail.fr>
2020-11-23 15:09:21 +0100

    added --use flag on eval command

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,11 +1,36 @@
 import discordEval from "discord-eval.ts"
+import cp from "child_process"
+import util from "util"
 import * as app from "../app"
+
+const exec = util.promisify(cp.exec)
+const regex = /--(?:install|use|add|with|npm) +([a-z-_.]+)(?:, ?([a-z-_.]+))*/i
+
+const packageJson = require("../../package.json")
 
 const command: app.Command = {
   name: "js",
   botOwner: true,
   aliases: ["eval", "code", "run", "="],
   async run(message) {
+    const match = regex.exec(message.content)
+    const packages = []
+
+    if (match) {
+      message.content = message.content.replace(regex, "").trim()
+      for (const pack of match.slice(1)) {
+        if (
+          !packageJson.dependencies.hasOwnProperty(pack) &&
+          !packageJson.devDependencies.hasOwnProperty(pack)
+        ) {
+          try {
+            await exec(`npm i ${pack}`)
+            packages.push(pack)
+          } catch (error) {}
+        }
+      }
+    }
+
     if (
       message.content.split("\n").length === 1 &&
       !/const|let|return/.test(message.content)
@@ -13,7 +38,14 @@ const command: app.Command = {
       message.content = "return " + message.content
     }
 
-    return discordEval(message.content, message)
+    await discordEval(message.content, message)
+
+    for (const pack of packages) {
+      try {
+        await exec(`npm remove --purge ${pack}`)
+        packages.push(pack)
+      } catch (error) {}
+    }
   },
 }
 
