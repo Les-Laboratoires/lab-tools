a10d1f09ee93acc8a6dfec77eade8c1bc4aca2b4
CamilleAbella <camille.abella@hotmail.fr>
2020-11-23 20:35:48 +0100

    show own scores with command !c

--- src/commands/counter.ts
+++ src/commands/counter.ts
@@ -2,43 +2,55 @@ import * as app from "../app"
 
 const command: app.Command = {
   name: "counter",
-  aliases: ["count", "score", "point", "pt", "c"],
+  aliases: ["count", "score", "point", "pt", "c", "sc"],
   async run(message) {
     if (message.content.startsWith("list")) {
       if (message.content.includes("--debug")) {
         await message.channel.send(
-          JSON.stringify(Array.from(app.scores.entries())).slice(0, 2023)
+          JSON.stringify(Array.from(app.scores.entries())).slice(0, 1999)
         )
       }
+
       const lines = await Promise.all(
         app.counters.map(async (counter) => {
           return `**${counter.name}** - [${counter.type}] - \`${counter.target}\``
         })
       )
+
       return message.channel.send(lines.join("\n"))
     } else if (message.content.startsWith("add")) {
       if (message.guild.ownerID !== message.author.id) {
         return message.channel.send("Raté, ya que Ghom qui peut faire ça !")
       }
+
       message.content = message.content.replace("add", "").trim()
+
       const [name, type, target] = message.content.split(/\s+/)
+
       if (!name || (type !== "match" && type !== "react") || !target) {
         return message.channel.send(
           "Raté, ça s'utilise comme ça:\n`!counter add [name] match|react [pattern|emoji]`"
         )
       }
+
       app.counters.set(name, { name, type, target })
+
       return message.channel.send(`Le compteur de ${name} a bien été ajouté.`)
     } else if (message.content.startsWith("remove")) {
       const [, name] = message.content.split(/\s+/)
+
       if (!name) return message.channel.send("Il manque le nom du compteur...")
+
       if (!app.counters.has(name))
         return message.channel.send(`Le compteur de ${name} n'existe pas.`)
+
       app.counters.delete(name)
+
       app.scores.forEach((score, id) => {
         delete score[name]
         app.scores.set(id, score)
       })
+
       return message.channel.send(
         `Ok le compteur de ${name} a bien été supprimé.`
       )
@@ -67,7 +79,18 @@ const command: app.Command = {
           .setDescription(leaderboard)
       )
     } else {
-      return message.channel.send("Hmmmmmmm <:gneuh:557124850486083605>")
+      const score = app.scores.ensure(message.author.id, {})
+      return message.channel.send(
+        new app.MessageEmbed()
+          .setTitle(`Scores | ${message.author.tag}`)
+          .setDescription(
+            app.code(JSON.stringify(score, null, 2).replace(/"/g, ""), "js")
+          )
+          .addField(
+            "total",
+            `**${eval(Object.values(score).join(" + "))}** points.`
+          )
+      )
     }
   },
 }
