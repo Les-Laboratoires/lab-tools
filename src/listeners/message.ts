1d366f4a5d524f82c0f0a49d38c8c1212ed0d8b4
CamilleAbella <CamilleAbella@users.noreply.github.com>
2020-12-15 10:40:08 +0000

    Prettified Code!

--- src/listeners/message.ts
+++ src/listeners/message.ts
@@ -105,12 +105,14 @@ const listener: app.Listener<"message"> = {
 
     if (cmd.needMoney) {
       const userMoney = app.money.ensure(message.author.id, 0)
-      if(userMoney < cmd.needMoney) {
+      if (userMoney < cmd.needMoney) {
         return message.channel.send(
           new app.MessageEmbed()
             .setColor("RED")
             .setAuthor(
-              `You don't have enough money to do this. You need ${Math.abs(userMoney - cmd.needMoney)}${app.currency} more.`,
+              `You don't have enough money to do this. You need ${Math.abs(
+                userMoney - cmd.needMoney
+              )}${app.currency} more.`,
               message.client.user?.displayAvatarURL()
             )
         )
