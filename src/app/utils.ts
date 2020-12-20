5f4b2cc0c769439555b2a23fe3159815db323345
Loockeeer <34001773+loockeeer@users.noreply.github.com>
2020-12-20 14:13:58 +0100

    Fix issue

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -118,7 +118,7 @@ export function leaderItem(
   const position = String(i + 1)
   return `\`# ${position}${position.length === 1 ? " " : ""} | ${
     obj.score
-  } ${typeName}\` ${" ".repeat(maxLen - obj.score)}- <@${obj.id}>`
+  } ${typeName}\`${" ".repeat(maxLen - obj.score + 1)}- <@${obj.id}>`
 }
 
 dayjs.locale("fr")
