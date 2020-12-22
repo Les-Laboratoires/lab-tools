ba06b620aed11786a1339f7829b095e8889172ab
Loockeeer <34001773+loockeeer@users.noreply.github.com>
2020-12-23 00:51:45 +0100

    Add combo & fix daily wait time

--- src/commands/daily.ts
+++ src/commands/daily.ts
@@ -6,14 +6,20 @@ const command: app.Command = {
   aliases: ["dl", "day"],
   async run(message) {
     if (message.author.bot) return
+    
+    const today = app.dayjs()
+    const lastDay = app.dayjs(app.daily.ensure(message.author.id, today.toObject(), "last"))
 
-    const lastDay = app.daily.ensure(message.author.id, -1)
-    const today = app.dayjs().date()
+    const lasted = today.diff(lastDay)
+    if (lasted > 8.64e+7) {
+      app.daily.set(message.author.id, today, "last")
+      
+      if (lasted < 1.728e+8) app.daily.inc(message.author.id, "combo")
+      else app.daily.set(message.author.id, 1, "combo")
 
-    if (lastDay !== today) {
-      app.daily.set(message.author.id, today)
+      const combo = app.daily.ensure(message.author.id, 1, "combo")
 
-      const gain = Math.round(10 + Math.random() * 10)
+      const gain = Math.round(10*Math.min(combo, app.maxcombo) + Math.random() * Math.min(combo, app.maxcombo))
 
       const success = await app.transaction("bank", [message.author.id], gain)
 
@@ -34,7 +40,7 @@ const command: app.Command = {
         .set("minute", 0)
         .set("millisecond", 0)
 
-      const timeout = midnight.valueOf() - Date.now()
+      const timeout = midnight.diff(new Date()).valueOf()
 
       return message.channel.send(
         `Nope ! Faut attendre ${tims.duration(timeout, {
