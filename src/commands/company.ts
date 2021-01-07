2293ad79e28f70bb04e8a8772d5d797390032ede
loockeeer <lulu.r1313@gmail.com>
2021-01-07 17:21:58 +0100

    Implement create and remove company commands

--- src/commands/company.ts
+++ src/commands/company.ts
@@ -12,10 +12,47 @@ const command: app.Command = {
     ])
     
     switch(key) {
+        case "create": {
+          if(app.companies.find('ownerID', message.author.id)) {
+            return message.channel.send('Kestufou t\'as déjà une entreprise !')
+          }
+          const companyName = app.getArgument(message)
+          if(!companyName) return message.channel.send('Faut renseigner le nom de ton entreprise du con')
+          const description = app.getArgument(message, "rest")
+          const company = {
+            name: companyName,
+            description,
+            ownerID: message.author.id,
+            money: 0
+          } as app.Company
+          app.companies.set(companyName, company)
+          return message.channel.send('Ton entreprise a été crée, jeune entrepreneur !')
+        }
+
+        case "remove": {
+          const company = app.companies.find('ownerID', message.author.id)
+          if(!company) {
+            return message.channel.send('Je peux pas supprimer ton entreprise si t\'en a pas...')
+          }
+          message.channel.send('Pour confirmer, envoie `ok`, sinon, envoie `stop`')
+          const collector = message.channel.createMessageCollector(() => true, { time: 60000 })
+          collector
+            .on('collect', m => {
+              switch(m.content) {
+                case "ok":
+                  app.companies.delete(company.name)
+                  return message.channel.send('Ok, bye bye ' + company.name)
+                case "stop":
+                  collector.stop()
+                  return message.channel.send('Opération annulée !')
+              }
+            })
+            .on('end', (_, reason) => 
+              reason === "time" && message.channel.send('Trop lent, j\'annule la procédure')
+            )
+        }
         case "list":
-        case "create":
-        case "remove":
-        case "join":
+        case "add":
         default:
             return message.channel.send(`Not yet implemented`)
     }
