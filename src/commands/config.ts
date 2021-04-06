71d326915d9c9dad5c1d36262a3b6fe67074a1d4
CamilleAbella <camille.abella@hotmail.fr>
2021-04-06 09:43:43 +0200

    add config command

---
+++ src/commands/config.ts
@@ -0,0 +1,63 @@
+import * as app from "../app"
+
+import guilds from "../tables/guilds"
+
+const command: app.Command<app.GuildMessage> = {
+  name: "config",
+  guildOnly: true,
+  description: "Display guild configs",
+  async run(message) {
+    return message.channel.send(
+      new app.MessageEmbed()
+        .setAuthor(
+          `${message.guild.name} | Configs`,
+          message.guild.iconURL({ dynamic: true }) ?? undefined
+        )
+        .setDescription(
+          app.CODE.stringify({
+            lang: "json",
+            content: JSON.stringify(
+              await guilds.query.select().where("id", message.guild.id).first(),
+              null,
+              2
+            ),
+          })
+        )
+    )
+  },
+  subs: [
+    {
+      name: "set",
+      guildOnly: true,
+      guildOwner: true,
+      description: "Set guild config",
+      positional: [
+        {
+          name: "name",
+          required: true,
+          description: "The name of edited property",
+        },
+        {
+          name: "value",
+          required: true,
+          description: "The value of edited property",
+        },
+      ],
+      async run(message) {
+        await guilds.query
+          .update({
+            [message.args.name]: message.args.value,
+          })
+          .where("id", message.guild.id)
+
+        return message.channel.send(
+          `${message.client.emojis.resolve(
+            app.Emotes.CHECK
+          )} Successfully updated \`${message.args.name}\` value. `
+        )
+      },
+    },
+  ],
+}
+
+module.exports = command
