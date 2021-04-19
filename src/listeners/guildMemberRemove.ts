d4d7dcd23ff15eac07d6c5d31ec59bf0d0f93cde
Ghom <camille.abella@hotmail.fr>
2021-04-19 14:33:13 +0200

    move presentation in logs on member leave

--- src/listeners/guildMemberRemove.ts
+++ src/listeners/guildMemberRemove.ts
@@ -7,17 +7,19 @@ const listener: app.Listener<"guildMemberRemove"> = {
 
     const logChannel = member.guild.channels.cache.get(app.logChannel)
 
-    if (logChannel && logChannel.isText())
-      logChannel
-        .send(
-          `${member.user?.tag ?? member.displayName} vient de nous quitter!`
-        )
-        .catch()
+    if (!logChannel || !logChannel.isText()) return
+      
+    logChannel
+      .send(
+        `${member.user?.tag ?? member.displayName} vient de nous quitter!`
+      )
+      .catch()
 
     if (presentations && presentations.isText()) {
       presentations.messages.fetch().then((messages) => {
         for (const [, message] of messages) {
           if (message.author.id === member.id) {
+            logChannel.send(message.content).catch()
             message.delete().catch()
             break
           }
