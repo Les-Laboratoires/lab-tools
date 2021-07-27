92640a97a36901373d67a7a06dccc584543c0616
CamilleAbella <camille.abella@hotmail.fr>
2021-07-27 13:09:09 +0200

    add approve command

---
+++ src/commands/approve.ts
@@ -0,0 +1,31 @@
+import * as app from "../app"
+
+export default new app.Command({
+  name: "apply",
+  description: "Approve a member",
+  middlewares: [app.staffOnly()],
+  channelType: "guild",
+  rest: {
+    name: "presentation",
+    description: "The presentation of approved member",
+  },
+  positional: [
+    {
+      name: "target",
+      description: "Target member",
+      required: true,
+      castValue: "member",
+    },
+  ],
+  async run(message) {
+    const target: app.GuildMember = message.args.target
+
+    await app.approveMember(target, message.args.presentation || undefined)
+
+    return message.send(
+      `${app.emote(message, "CHECK")} Successfully approved **${
+        target.user.tag
+      }**.`
+    )
+  },
+})
