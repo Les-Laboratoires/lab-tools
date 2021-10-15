14d0d8e39a5cb20bb438e2da316c6c36da10cb58
CamilleAbella <camille.abella@hotmail.fr>
2021-10-15 10:44:11 +0200

    fetch presentations on start of bot and remove raw event

---
+++ src/listeners/ready.presentation.ts
@@ -0,0 +1,34 @@
+import * as app from "../app.js"
+
+const listener: app.Listener<"ready"> = {
+  event: "ready",
+  description: "A ready listener",
+  once: true,
+  async run() {
+    for (const guild of this.guilds.cache.values()) {
+      const config = await app.getConfig(guild, true)
+
+      if (config.presentation_channel_id) {
+        const channel = await guild.channels.fetch(
+          config.presentation_channel_id
+        )
+
+        if (!channel)
+          return app.error(
+            `${guild.name} presentation_channel_id not found`,
+            "ready.presentation"
+          )
+
+        if (!channel.isText())
+          return app.error(
+            `${guild.name} presentation_channel_id is not a TextBasedChannel`,
+            "ready.presentation"
+          )
+
+        await channel.messages.fetch({ limit: 100 })
+      }
+    }
+  },
+}
+
+export default listener
