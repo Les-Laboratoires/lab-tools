6bd7677372a2549c76b8a02ff03c3e0c55394402
CamilleAbella <camille.abella@hotmail.fr>
2022-01-03 16:35:10 +0100

    fix cron table name column

--- src/tables/cron.ts
+++ src/tables/cron.ts
@@ -9,6 +9,9 @@ export default new app.Table<{
 }>({
   name: "cron",
   description: "Represent saved cron",
+  migrations: {
+    1: table => table.unique( ["name"])
+  },
   setup: (table) => {
     table
       .string("user_id")
