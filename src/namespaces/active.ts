774dfe9bac5de10135e00d04af7161dabc583f7b
Ghom <camille.abella@hotmail.fr>
2023-10-10 22:36:18 +0200

    updated isActive method

--- src/namespaces/active.ts
+++ src/namespaces/active.ts
@@ -5,17 +5,19 @@ import messages from "../tables/message.js"
 export async function isActive(
   member: app.GuildMember,
   period = 1000 * 60 * 60 * 24 * 7,
-  messageCount = 50
+  requiredMessageCount = 50
 ): Promise<boolean> {
-  // Get the user id
-
   const user = await app.getUser(member, true)
   const guild = await app.getGuild(member.guild, true)
 
-  const rowsInPeriod = await messages.query
+  const data = await messages.query
     .where("author_id", user._id)
     .where("guild_id", guild._id)
     .where("created_timestamp", ">", Date.now() - period)
+    .select("count(*) as messageCount")
+    .first()
 
-  return rowsInPeriod.length > messageCount
+  if(!data) return false
+
+  return data.messageCount > requiredMessageCount
 }
