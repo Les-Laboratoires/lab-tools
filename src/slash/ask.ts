8ff26afb09df927bd11d5195065f75fc784912c3
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-10-27 13:49:22 +0200

    added slash command for asking points in thread channel

---
+++ src/slash/ask.ts
@@ -0,0 +1,63 @@
+import * as app from "../app.js"
+
+export default new app.SlashCommand({
+  name: "ask",
+  description: "Ask points to a member",
+  async run(interaction) {
+    if (!interaction.channel || !interaction.channel.isThread())
+      return interaction.reply({
+        content: "This command can only be used in a thread channel.",
+        ephemeral: true,
+      })
+
+    if (!interaction.guild)
+      return interaction.reply({
+        content: "This command can only be used in a guild.",
+        ephemeral: true,
+      })
+
+    if (interaction.user.id === interaction.channel.ownerId)
+      return interaction.reply({
+        content: "You can't ask points to yourself.",
+        ephemeral: true,
+      })
+
+    const target = (await interaction.channel.fetchOwner({
+      force: true,
+      cache: false,
+    }))!
+
+    await interaction.reply({
+      embeds: [
+        new app.MessageEmbed()
+          .setTitle(`Notez l'aide de ${interaction.user.username}`)
+          .setDescription(
+            `Vous pouvez attribuer des points à ${
+              interaction.member
+            } en fonction de la qualité de l'aide apportée en cliquant sur le bouton souhaité. Vous pouvez également noter la personne avec la commande \`${await app.prefix(
+              interaction.guild,
+            )}note @${interaction.user.username} <1..5>\``,
+          ),
+      ],
+      components: [
+        new app.MessageActionRow().addComponents(
+          new app.MessageButton()
+            .setCustomId(`point;10;${target.id};${interaction.user.id}`)
+            .setLabel("Très bien")
+            .setStyle("PRIMARY")
+            .setEmoji("👍"),
+          new app.MessageButton()
+            .setCustomId(`point;15;${target.id};${interaction.user.id}`)
+            .setLabel("Excellent!")
+            .setStyle("PRIMARY")
+            .setEmoji(interaction.client.emojis.resolve("507420549765529610")!),
+        ),
+      ],
+    })
+
+    await app.sendLog(
+      interaction.guild,
+      `${interaction.user} ask points to ${target.user} in ${interaction.channel}.`,
+    )
+  },
+})
