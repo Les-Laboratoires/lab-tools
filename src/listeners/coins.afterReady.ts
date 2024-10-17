d30f70ac64d3ecb3c877f3674bd92dd857bd4dc9
GhomKrosmonaute <camille.abella@hotmail.fr>
2024-10-17 03:03:36 +0200

    updated the coins gain algo (with active members message count)

--- src/listeners/coins.afterReady.ts
+++ src/listeners/coins.afterReady.ts
@@ -3,6 +3,7 @@ import * as app from "#app"
 import userTable from "#tables/user.ts"
 import pointTable from "#tables/point.ts"
 import noteTable from "#tables/rating.ts"
+import messageTable from "#tables/message.ts"
 
 const listener: app.Listener<"afterReady"> = {
   event: "afterReady",
@@ -16,6 +17,9 @@ const listener: app.Listener<"afterReady"> = {
           coins: number
           points: `${number}`
           rating: `${number}`
+          givenNotes: `${number}`
+          active: number | null
+          messages: number
         }[] = await userTable.query
           .leftJoin(
             pointTable.query
@@ -35,14 +39,35 @@ const listener: app.Listener<"afterReady"> = {
             "note_totals.to_id",
             "user._id",
           )
+          .leftJoin(
+            noteTable.query
+              .select("from_id")
+              .count("* as givenNotes")
+              .groupBy("from_id")
+              .as("note_totals"),
+            "note_totals.from_id",
+            "user._id",
+          )
+          .leftJoin("active", "active.user_id", "user._id")
+          .leftJoin(
+            messageTable.query
+              .select("author_id")
+              .count("* as messages")
+              .groupBy("author_id")
+              .as("message_totals"),
+            "message_totals.author_id",
+            "user._id",
+          )
           .select(
             "user._id",
             "user.coins",
             "point_totals.points",
             "note_totals.rating",
+            "active.user_id as active",
+            "message_totals.messages",
           )
           .where("point_totals.points", ">", 0)
-          .where("note_totals.rating", ">", 0)
+          .or.where("note_totals.rating", ">", 0)
 
         // update the coins of each user
         await userTable.query
@@ -50,7 +75,12 @@ const listener: app.Listener<"afterReady"> = {
             users.map((user) => ({
               _id: user._id,
               coins: Math.ceil(
-                user.coins + +user.points * Math.max(1, +user.rating),
+                user.coins +
+                  +user.points * Math.max(1, +user.rating) +
+                  +user.givenNotes * 5 +
+                  (user.active
+                    ? Math.max(10, Math.floor(user.messages * 0.001))
+                    : 0),
               ),
             })),
           )
