9785f121b0d38121de46cf5d2b5d212ca828835f
GhomKrosmonaute <camille.abella@hotmail.fr>
2024-10-24 22:31:19 +0200

    formated profile number values

--- src/slash/profile.ts
+++ src/slash/profile.ts
@@ -19,6 +19,10 @@ export default new app.SlashCommand({
 
     const pointRank = await app.getPointRank(interaction.user)
 
+    const messagesSent = await messageTable.cache.count(
+      `author_id = ${user._id} AND guild_id = ${guild._id}`,
+    )
+
     return interaction.reply({
       embeds: [
         new app.EmbedBuilder()
@@ -26,13 +30,11 @@ export default new app.SlashCommand({
           .setThumbnail(interaction.user.displayAvatarURL())
           .setDescription(
             (pointRank
-              ? `Helper rank: **#${pointRank.rank}** (**${user.points}** pts)\n`
+              ? `Helper rank: **#${pointRank.rank}** (**${user.points.toLocaleString()}** pts)\n`
               : "") +
-              `Total money: **${user.coins}** 🪙\n` +
-              `Hourly money: **${Math.floor(app.getUserHourlyCoins(user))}** 🪙\n` +
-              `Messages sent: **${await messageTable.cache.count(
-                `author_id = ${user._id} AND guild_id = ${guild._id}`,
-              )}**`,
+              `Total money: **${user.coins.toLocaleString()}** 🪙\n` +
+              `Hourly money: **${Math.floor(app.getUserHourlyCoins(user)).toLocaleString()}** 🪙\n` +
+              `Messages sent: **${messagesSent.toLocaleString()}**`,
           )
           .setFields(
             {
